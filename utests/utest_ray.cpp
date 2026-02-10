66646ec317c058cbd1e9de601e1cdc389f8ae97c
stroseth <stroseth11@gmail.com>
2026-02-10 14:10:46 -0600

    ray: finished utests, all tests pass

--- utests/utest_ray.cpp
+++ utests/utest_ray.cpp
@@ -21,14 +21,37 @@ TEST_CASE("#1 - Default Constructor")
     REQUIRE_THAT(defaultRay.direction().z(), Catch::Matchers::WithinAbs(actualDefault, eps));
 }
 
-/*TEST_CASE("#2 - Origin + Direction")
+TEST_CASE("#2 - Origin + Direction")
 {
     point3 custOrig = point3();
     vec3 custDir = vec3(.2f,.6f,1.0f);
     ray customRay = ray(custOrig, custDir);
+
+    float eps = 1.0e-3;
+
+    REQUIRE_THAT(customRay.origin().x(), Catch::Matchers::WithinAbs(0.0f, eps));
+    REQUIRE_THAT(customRay.origin().y(), Catch::Matchers::WithinAbs(0.0f, eps));
+    REQUIRE_THAT(customRay.origin().z(), Catch::Matchers::WithinAbs(0.0f, eps));
+
+    //dir
+    REQUIRE_THAT(customRay.direction().x(), Catch::Matchers::WithinAbs(.2f, eps));
+    REQUIRE_THAT(customRay.direction().y(), Catch::Matchers::WithinAbs(.6f, eps));
+    REQUIRE_THAT(customRay.direction().z(), Catch::Matchers::WithinAbs(1.0f, eps));
+
 }
 
 TEST_CASE("#3 - Parametric Ray Equation")
 {
-    //Require...something
-}*/
+    point3 custOrig = point3();
+    vec3 custDir = vec3(.6f,.12f,1.0f);
+    ray customRay = ray(custOrig, custDir);
+
+    float eps = 1.0e-3;
+
+    point3 ans = customRay.at(2.4);
+    point3 actualAns = point3(1.44f,.288f,2.4f);
+
+    REQUIRE_THAT(ans.x(), Catch::Matchers::WithinAbs(actualAns.x(), eps));
+    REQUIRE_THAT(ans.y(), Catch::Matchers::WithinAbs(actualAns.y(), eps));
+    REQUIRE_THAT(ans.z(), Catch::Matchers::WithinAbs(actualAns.z(), eps));
+}
