9df4dbe96a73819c1ac55a46ec4f174648d8211b
stroseth <stroseth11@gmail.com>
2026-02-12 10:16:35 -0600

    Camera: fixes on camera. non-working ray to color in main... :(

--- saralib/Camera.h
+++ saralib/Camera.h
@@ -1,13 +1,13 @@
 //base class for camera. Contains info on how scene should be rendered
-
 #pragma once
+
+#include "ray.h"
 
 class Camera{
     public:
         //default camera
         Camera(); // default pixel img dimensions are 100x100
         Camera(int pixel_nx, int pixel_ny); //input pixel image dimensions
-        virtual -Camera(){}
 
         virtual ray generateRay(int i, int j) = 0;
 
@@ -18,7 +18,7 @@ class Camera{
         vec3 U,V,W;
 
         float focalLength; //d or distance. can pick whatever unit but be consistent with it.
-        float imageplane_width, imageplane_height; //how big is the img plane
+        float imagePlane_width, imagePlane_height; //how big is the img plane
 
         int nx, ny;
-}
+};
--- saralib/fbMain.cpp
+++ saralib/fbMain.cpp
@@ -1,14 +1,33 @@
 #include "Framebuffer.h"
 #include "vec3.h"
+#include "PerspectiveCamera.h"
 
 using color = vec3;
 
 int main(int argc , char* argv []) {
-    Framebuffer fb1{600,600};
-    color blue = color(.4f, .223f, .991f); //color must be floats
+    color blue = color(.4f, .223f, .991f);
+    color green = color(.0f, .8f, .0f);
+    /*Framebuffer fb1{600,600};
+     //color must be floats
     //fb1.clearToColor(blue);
     //fb1.exportAsPNG("fb1.png");
-    color green = color(.0f, .8f, .0f); //geen
     fb1.clearToGradient(blue, green);
     fb1.exportAsPNG("HopefullyAGradient.png");
+    */
+
+    Framebuffer fb;
+    fb.clearToColor(blue);
+    
+    // set default cam variables here
+    PerspectiveCamera p = PerspectiveCamera();
+    // PerspectiveCamera p( origin , viewdir , focalLength ,
+    // imagePlaneWidth , fb.width(), fb.height () );
+    for (int x=0; x <800; ++x) {
+        for (int y=0; y <800; ++y) {
+            ray r = p.generateRay(x, y);
+            //color ray_dir_color = 0.5*(r.direction() + 1.0f); // this line doesn't work. 
+            fb.setPixelColor(x, y, ray_dir_color );
+        }
+    }
+    fb.exportAsPNG( "defaultCamRayColors.png" );
 }
