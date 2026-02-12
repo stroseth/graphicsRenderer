6f0057b8e218e1e2e667184e451b43fff6982975
stroseth <stroseth11@gmail.com>
2026-02-12 09:04:43 -0600

    Camera: added default camera, base of perspective camera

--- saralib/Camera.h
+++ saralib/Camera.h
@@ -5,22 +5,20 @@
 class Camera{
     public:
         //default camera
-        Camera() : pos(0,0,0),
-            u(1,0,0), v(0,1,0), w(0,0,1),
-            focalLength(1.0), //one = one unit of my choice
-            imageplane_width(0.25), imageplane_height(0.25)
-        {}
+        Camera(); // default pixel img dimensions are 100x100
+        Camera(int pixel_nx, int pixel_ny); //input pixel image dimensions
         virtual -Camera(){}
 
         virtual ray generateRay(int i, int j) = 0;
 
     protected:
+        vec3 pos; //camera position
 
-    vec3 pos;
+        //basis vectors for camera
+        vec3 U,V,W;
 
-    //basis vectors for camera
-    vec3 u,v,w;
+        float focalLength; //d or distance. can pick whatever unit but be consistent with it.
+        float imageplane_width, imageplane_height; //how big is the img plane
 
-    float focalLength; //d or distance. can pick whatever unit but be consistent with it.
-    float imageplane_width, imageplane_height; //how big is the img plane
+        int nx, ny;
 }
