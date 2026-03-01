e53d8c714cc8c4c34a8dbdaf93b5875ee28ce20d
stroseth <stroseth11@gmail.com>
2026-02-28 21:10:45 -0600

    Shape: added Shape base class

--- saralib/Shape.h
+++ saralib/Shape.h
@@ -1,11 +1,13 @@
+#pragma once
+
+#include "HitStruct.h"
+#include "ray.h"
+#include "vec3.h"
+
 //Base class for things like da spheres and allat
 //Has the shader information in it.
-//Could be an interface or abstract class'' pure virtual means it dnt do nothin untli we define it
-
-/*
 class Shape{
 public:
-
-    virtual bool intersect(smth) = 0;
-}
-*/
+    virtual bool intersect(const ray& r, float t_min, float& t_max, HitStruct& hit) const = 0;
+    virtual getColor() const = 0;
+};
