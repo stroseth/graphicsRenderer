9ced88eba47610a0f1fa304fa7a7ad944b700c24
stroseth <stroseth11@gmail.com>
2026-03-03 14:49:08 -0600

    main: generates an odd shape now. what?

--- saralib/fbMain.cpp
+++ saralib/fbMain.cpp
@@ -1,20 +1,51 @@
+#include <memory>
+
 #include "Framebuffer.h"
 #include "vec3.h"
 #include "PerspectiveCamera.h"
+#include "ray.h"
+#include "Shape.h"
+#include "Sphere.h"
+#include "HitStruct.h"
 
 using color = vec3;
 
 int main(int argc , char* argv []) {
-    color blue = color(.4f, .223f, .991f);
-    color green = color(.0f, .8f, .0f);
-    /*Framebuffer fb1{600,600};
-     //color must be floats
-    //fb1.clearToColor(blue);
-    //fb1.exportAsPNG("fb1.png");
-    fb1.clearToGradient(blue, green);
-    fb1.exportAsPNG("HopefullyAGradient.png");
-    */
+    //void rayToColor(); //ray-to-color funct
 
+    //FB and colors
+    Framebuffer fb0{600, 300};
+    color sphereColor(1,0,0);
+    color bg(1,1,1);
+
+    //Camera setup
+     PerspectiveCamera p(vec3(0, 0, 0), vec3(0, 0, -1), 0.7, 0.5, 0.5, fb0.getFbWidth(), fb0.getFbHeight());
+  
+    //Sphere Info
+    std::shared_ptr<Shape> s = std::make_shared<Sphere>(point3(0, -1.25, -7), 0.3f, vec3(1,1,1));
+
+    float tmin = 0.001f;
+    float tmax = std::numeric_limits<float>::infinity();
+    HitStruct h;
+
+    for (int x = 0; x < fb0.getFbWidth(); ++x){
+        for (int y=0; y<fb0.getFbHeight(); ++y){
+            ray r = p.generateRay(x,y);
+
+            if (s->intersect(r,tmin, tmax, h)){
+                fb0.setPixelColor(x,y, sphereColor);
+            }
+            else{
+                fb0.setPixelColor(x,y,bg);
+            };
+        };
+    };
+
+    fb0.exportAsPNG("Flag_maybe.png");
+}
+
+/*
+void rayToColor(){
     Framebuffer fb{200,200};
     //fb.clearToColor(blue);
     
@@ -24,7 +55,7 @@ int main(int argc , char* argv []) {
     float widHeight = .5f;
 
     // set default cam variables here
-    PerspectiveCamera p = PerspectiveCamera( org , view , focal ,widHeight , widHeight, 200, 200 );
+    PerspectiveCamera p = PerspectiveCamera( org , view , focal ,fb.getFbWidth(), fb.getFbHeight(), 200, 200 );
     for (int x=0; x <200; ++x) {
         for (int y=0; y <200; ++y) {
             ray r = p.generateRay(x, y);
@@ -35,3 +66,4 @@ int main(int argc , char* argv []) {
     }
     fb.exportAsPNG( "defaultCamRayColors.png" );
 }
+*/
