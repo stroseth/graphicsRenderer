d1f9d049031ff4bed76eac24d6775820d197fac0
stroseth <stroseth11@gmail.com>
2026-04-21 15:33:34 -0500

    Antialiasing works!

--- saralib/fbMain.cpp
+++ saralib/fbMain.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <memory>
 #include <limits>
+#include <random>
+
 #include "Framebuffer.h"
 #include "PerspectiveCamera.h"
 #include "ray.h"
@@ -14,6 +16,12 @@
 #include "BlinnPhongShader.h"
 #include "RayTracer.h"
 #include "MirrorShader.h"
+
+float randomOffset(){
+  static std::uniform_real_distribution<float> distribution(0.0, 1.0);
+  static std::mt19937 generator;
+  return distribution(generator);
+}
 
 int main(int argc, char *argv[])
 {
@@ -41,16 +49,31 @@ int main(int argc, char *argv[])
   std::vector<std::shared_ptr<PointLight>> lights;
   lights.push_back(std::make_shared<PointLight>());
 
+  //antialiasing start
+  int rpp_NSquare = 4;
+  int maxDepth = 2;
 
   for (int x = 0; x < fb.getFbWidth(); x++) {
     for (int y = 0; y < fb.getFbHeight(); y++) {
-      ray r = cam.generateRay(x, y);
-      vec3 pixelColor = computeRayColor(r, shapes, lights, 2);
+      vec3 accumulatedColor(0.0, 0.0, 0.0);
+
+      //stratified sampling
+      for (int p = 0; p < rpp_NSquare; p++){
+        for (int q = 0; q < rpp_NSquare; q++){
+          float pOffset = (p + randomOffset()) / rpp_NSquare;
+          float qOffset = (q + randomOffset()) / rpp_NSquare;
+
+          ray r = cam.generateRay(x + pOffset, y + qOffset);    
+          accumulatedColor += computeRayColor(r, shapes, lights, maxDepth);
+        }
+      }
+
+      vec3 pixelColor = accumulatedColor / (float)(rpp_NSquare * rpp_NSquare);
       fb.setPixelColor(x, y, pixelColor);
     }
   }
 
-  fb.exportAsPNG("mirrorTest.png");
+  fb.exportAsPNG("4antialias.png");
 
   return 0;
 }
