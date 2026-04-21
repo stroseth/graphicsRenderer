bd2d98617f725dcfa87b03360fd94ac0a03c9710
stroseth <stroseth11@gmail.com>
2026-04-21 12:54:52 -0500

    Camera: added getposition funct

--- saralib/fbMain.cpp
+++ saralib/fbMain.cpp
@@ -39,38 +39,39 @@ vec3 computeRayColor(const ray &r, const std::vector<std::shared_ptr<Shape>> &sh
     if (shader) {
       return shader->rayColor(closestHit, lights);
     } else {
-      BlinnPhongShader defaultShader;
+      NormalShader defaultShader;
       return defaultShader.rayColor(closestHit, lights);
     }
   }
 
   // Background color
-  //vec3 unit_direction = unit_vector(r.direction());
-  //auto a = 0.5 * (unit_direction.y() + 1.0);
-  //return (1.0 - a) * vec3(1.0, 1.0, 1.0) + a * vec3(0.5, 0.7, 1.0);
+  vec3 unit_direction = unit_vector(r.direction());
+  auto a = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - a) * vec3(1.0, 1.0, 1.0) + a * vec3(0.5, 0.7, 1.0);
   return vec3(1,1,1);
 }
 
 int main(int argc, char *argv[])
 {
-  Framebuffer fb(200, 200);
+  Framebuffer fb(400, 400);
 
   std::vector<std::shared_ptr<Shape>> shapes;
 
   // Default Camera: focal length = 0.7, image plane width = 0.5
-   PerspectiveCamera cam(vec3(0, 0, 0), vec3(0, 0, -1), 1.0, 0.5, 0.5, 200, 200);
+  PerspectiveCamera cam(vec3(0, 0, 0), vec3(0, 0, -1), 1.0, 0.5, 0.5, 400, 400);
+
+  //create shaders
+  auto lambertianShader = std::make_shared<LambertianShader>();
+  auto blinnPhongShader = std::make_shared<BlinnPhongShader>();
+  blinnPhongShader->setEyePosition(cam.getPosition());
 
   //Make shapes
-  shapes.push_back(std::make_shared<Triangle>(
-    vec3(-0.15f,-0.15f,-1.5f), vec3(0.15f,-0.15f,-1.5f), vec3(0.0f,0.15f,-1.5f),
-    vec3(1.0, 0.0, 0.7)));
-
-  shapes.push_back(std::make_shared<Sphere>(vec3(.33f,-.023f, -2.1f), 0.5f, vec3(0.5f, 0, 0.5f)));
+  shapes.push_back(std::make_shared<Sphere>(
+    vec3(0, 0, -2.5), 0.5f, vec3(0.3, 0.8, 0.3), blinnPhongShader));
 
   //Make light
   std::vector<std::shared_ptr<PointLight>> lights;
   lights.push_back(std::make_shared<PointLight>());
-
 
 
   for (int x = 0; x < fb.getFbWidth(); x++) {
