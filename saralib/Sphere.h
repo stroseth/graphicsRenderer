b58af6eb5d9f02ae6be9861d920bf1a521fce36f
stroseth <stroseth11@gmail.com>
2026-04-21 12:54:44 -0500

    Sphere:  constructor takes shader

--- saralib/Sphere.h
+++ saralib/Sphere.h
@@ -13,6 +13,7 @@ class Sphere : public Shape
         Sphere(): center(vec3(0,0,0)), radius(1.0), color(vec3(1.0,1.0,1.0)) {}
         Sphere(vec3 c, float r): center(c), radius(r), color(vec3(1.0,1.0,1.0)) {}
         Sphere(vec3 c, float r, vec3 col): center(c), radius(r), color(col){}
+        Sphere(vec3 c, float r, vec3 col, std::shared_ptr<Shader> shd) : center(c), radius(r), color(col), shader(shd) {}
 
         bool intersect(const ray&r, float t_min, float& t_max, HitStruct& hit) const override;
         vec3 getColor() const override;
